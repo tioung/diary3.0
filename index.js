c5c38876640d2173fb74671b1398e7220e954ae6
davidkc <dakchoy@gmail.com>
2020-11-29 20:25:10 +0400

    second commit

--- index.js
+++ index.js
@@ -2,31 +2,64 @@
 const form=document.querySelector('form')
 const diary=document.querySelector('.diary')
 
+// form.addEventListener('submit',e=>{
+//   e.preventDefault()
+//
+//   //create a button for each entry
+//   const btn=document.createElement('button')
+//   const date = new Date()
+//   const dateString=`${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.toLocaleTimeString()}`
+//   btn.textContent=dateString
+//   btn.classList.add('btn')
+//   btn.classList.add('btn-entry')
+//   btn.addEventListener('click',()=>{
+//     div.classList.toggle('hide')
+//   })
+//   diary.appendChild(btn)
+//
+//   //create a div for each entry and hide it
+//   const div=document.createElement('div')
+//   div.textContent=form.textentry.value
+//   div.classList.add('hide')
+//   div.classList.add('btn')
+//   div.classList.add('text-entry')
+//   diary.appendChild(div)
+//
+//   form.textentry.value=''
+// })
+
+function displayBtns(){
+  for(let i=0; i<localStorage.length; i++){
+    //create a button for each entry
+    const btn=document.createElement('button')
+    const key=localStorage.key(i);
+    btn.textContent=key
+    btn.classList.add('btn')
+    btn.classList.add('btn-entry')
+    btn.addEventListener('click',()=>{
+      div.classList.toggle('hide')
+    })
+    diary.appendChild(btn)
+
+    //create a div for each entry and hide it
+    const div=document.createElement('div')
+    div.textContent=localStorage.getItem(key)
+    div.classList.add('hide')
+    div.classList.add('btn')
+    div.classList.add('text-entry')
+    diary.appendChild(div)
+  }
+}
+displayBtns()
 form.addEventListener('submit',e=>{
   e.preventDefault()
 
-  //create a button for each entry
-  const btn=document.createElement('button')
   const date = new Date()
   const dateString=`${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.toLocaleTimeString()}`
-  btn.textContent=dateString
-  btn.classList.add('btn')
-  btn.classList.add('btn-entry')
-  btn.addEventListener('click',()=>{
-    div.classList.toggle('hide')
-    // btn.classList.toggle('display')
-  })
-  diary.appendChild(btn)
+  const text=form.textentry.value
+  localStorage.setItem(dateString, text)
 
-  //create a div for each entry and hide it
-  const div=document.createElement('div')
-  div.textContent=form.textentry.value
-  div.classList.add('hide')
-  div.classList.add('btn')
-  div.classList.add('text-entry')
-  diary.appendChild(div)
-
+  location.reload()
+  // displayBtns()
   form.textentry.value=''
 })
-
-// console.log(form)
